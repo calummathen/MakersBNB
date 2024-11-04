a3945222f01cda5bcb1e8e72202b09f3dff10fde
Jamie <jamieplumy@gmail.com>
2024-11-04 13:45:48 +0000

    added user and test_user_model files with functionality

---
+++ lib/user.py
@@ -0,0 +1,16 @@
+class User():
+    def __init__(self, id, username, name, password, email, phone_number):
+        self.id = id
+        self.username = username
+        self.name = name
+        self.password = password
+        self.email = email
+        self.phone_number = phone_number
+        
+        
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+    
+    
+    def __repr__(self):
+        return f"User({self.id}, {self.username}, {self.name}, {self.password}, {self.email}, {self.phone_number})"
