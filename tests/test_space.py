4683e682325c6f30f4b81ba7d897aa032db8e575
Calum Mathen <calummathen@hotmail.co.uk>
2024-11-04 13:40:37 +0000

    Space Class Created

---
+++ tests/test_space.py
@@ -0,0 +1,20 @@
+from lib.space import Space
+
+def test_space_constructs(db_connection):
+    space = Space(1, "Cezary", "123 Fake Street", "A nice space", "200.50", ["2024-11-01", "2024-11-02"], 1)
+    assert space.id == 1
+    assert space.name == "Cezary"
+    assert space.address == "123 Fake Street"
+    assert space.description == "A nice space"
+    assert space.price == "200.50"
+    assert space.dates_booked == ["2024-11-01", "2024-11-02"]
+    assert space.owner_id == 1
+
+def test_album_formats_nicely(db_connection):
+    space = Space(1, "Cezary", "123 Fake Street", "A nice space", "200.50", ["2024-11-01", "2024-11-02"], 1)
+    assert str(space) == "Space(1, Cezary, 123 Fake Street, A nice space, 200.50, ['2024-11-01', '2024-11-02'], 1)"
+
+def test_albums_are_equal(db_connection):
+    space1 = Space(1, "Cezary", "123 Fake Street", "A nice space", "200.50", ['2024-11-01', '2024-11-02'], 1)
+    space2 = Space(1, "Cezary", "123 Fake Street", "A nice space", "200.50", ['2024-11-01', '2024-11-02'], 1)
+    assert space1 == space2
