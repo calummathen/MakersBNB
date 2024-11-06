de224f4648f1b9e4c8daaa6f7b216ea3734f551b
K-B <cezary.karwoskibudd@gmail.com>
2024-11-06 09:43:39 +0000

    Some house cleaning

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lib.space import Space
 from lib.user import User
 from lib.user_repository import UserRepository
 from dotenv import load_dotenv
+from helper_functions import protect_route
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -35,46 +36,8 @@ def get_index():
     spaces = repository.all()
     return render_template('home.html', spaces=spaces)
 
-# Route for Signing up
-@app.route('/signup', methods=['POST'])
-def sign_up():
-    connection = get_flask_database_connection(app)
-    repository = UserRepository(connection)
-    id = request.form["id"]
-    username = request.form["username"]
-    name = request.form["name"]
-    password = request.form["password"]
-    email = request.form["email"]
-    phone_number = request.form["phone_number"]
-    # Will need a check for a unique user
-    new_user = User(id, username, name, password, email, phone_number)
-    repository.create_user(new_user)
-    return render_template('login.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    connection = get_flask_database_connection(app)
-    repository = UserRepository(connection)
-    username = request.form["username"]
-    password = request.form["password"]
-    user = repository.login(username, password)
-    if not user:
-        error = 'Incorrect Username or Password'
-        return error
-        # return render_template('login.html', error=error)
-    if 'username' not in session:
-        session['id'] = user['id']
-        session['username'] = user['username']
-    return user
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return 'hello world'
-
-
-    
-    
+from authentication_routes import auth_routes
+auth_routes(app)
 
 @app.route('/new_space', methods=['GET'])
 def get_test_route():
@@ -85,10 +48,7 @@ def create_user():
     name = request.form.get('name')
     return render_template('temp_test.html',name=name)
 
-def protect_route():
-    if not session.get('id'):
-        return redirect(url_for('get_root'))
-    return None
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
