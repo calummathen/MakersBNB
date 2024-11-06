d6b326b3b4a34553fb27a7db9f8519d1b1f003a7
Calum Mathen <calummathen@hotmail.co.uk>
2024-11-06 14:12:35 +0000

    Single_space added with calendar function

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from lib.space_repository import SpaceRepository
 from lib.space import Space
 from lib.user import User
 from lib.user_repository import UserRepository
+from lib.booking import Booking
+from lib.booking_repository import BookingRepository
 from validation_methods import check_signup_valid
 from dotenv import load_dotenv
+from datetime import date
+import json
 
 
 # Create a new Flask app
@@ -36,6 +40,25 @@ def get_index():
     repository = SpaceRepository(connection)
     spaces = repository.all()
     return render_template('home.html', spaces=spaces)
+
+
+@app.route('/space/<int:id>', methods=['GET'])
+def display_single_space(id):
+    connection = get_flask_database_connection(app)
+    space_repository = SpaceRepository(connection)
+    space = space_repository.find(id)
+    booking_repository = BookingRepository(connection)
+    current_bookings = booking_repository.find_booking_for_space(id)
+    unavailable_dates = []
+    for booking in current_bookings:
+        dates = {
+            "from": booking.check_in.strftime('%Y-%m-%d'),
+            "to": booking.check_out.strftime('%Y-%m-%d')
+        }
+        unavailable_dates.append(dates)
+
+    return render_template('single_space.html', space=space, unavailable_dates=unavailable_dates)
+    
 
 # Route for Signing up
 # Commented out as don't believe it is needed anymore, but want to check:
@@ -77,8 +100,6 @@ def logout():
     session.clear()
     return 'hello world'
 
-
-    
     
 
 @app.route('/new_space', methods=['GET'])
