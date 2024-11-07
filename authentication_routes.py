70678a79447732126f899a990cffd8990e461487
Patrick Shannon <shannon.patrick06@gmail.com>
2024-11-07 13:46:18 +0000

    Navbar ACTUALLY fully functional

--- authentication_routes.py
+++ authentication_routes.py
@@ -19,7 +19,7 @@ def auth_routes(app):
             connection = get_flask_database_connection(app)
             repository = UserRepository(connection)
             repository.create_user(User(None, username, name, password, email, phone_number))
-            return redirect('get_login_page')
+            return render_template('login.html')
     
     @app.route('/login', methods=['POST'])
     def login():
