daea572a12c766d5f62096155291346d0fcf3d60
Patrick Shannon <shannon.patrick06@gmail.com>
2024-11-08 12:10:37 +0000

    Added new space validation, error messages

--- routes/new_spaces_routes.py
+++ routes/new_spaces_routes.py
@@ -3,6 +3,7 @@ from flask import request, render_template, session, redirect, url_for
 from lib.space_repository import SpaceRepository
 from lib.space import Space
 from helper_functions import protect_route
+from validation_methods import check_space_creation_valid
 
 def new_spaces_routes(app):
     @app.route('/space/new', methods=['GET'])
@@ -24,6 +25,10 @@ def new_spaces_routes(app):
         description = request.form['description']
         price = request.form['price']
         id = session['id']
-        space = Space(None, name, address, description, price, '[]', id)
-        space_repository.create(space)
-        return redirect(url_for('profile'))
+        errors = [error for error in check_space_creation_valid(name,address,description,price) if error != True]
+        if len(errors) != 0:
+            return render_template('space_form.html', errors=errors)
+        else:
+            space = Space(None, name, address, description, price, '[]', id)
+            space_repository.create(space)
+            return redirect(url_for('profile'))
